ffaca5fd2eaa8b0e569afe22a84816ad0e332949
CuddlyBunion341 <daniel@bengl.cz>
2022-07-13 21:41:36 +0200

    form styling

--- login.php
+++ login.php
@@ -6,9 +6,11 @@
 
 
 <form action="login.php" method="post">
-    <h1>Log In</h1>
-    <input type="text" name="username" id="username" placeholder="Username">
-    <input type="password" name="password" id="password" placeholder="Password">
+    <div class="container">
+        <h1>Log In</h1>
+        <input type="text" name="username" id="username" placeholder="Username">
+        <input type="password" name="password" id="password" placeholder="Password">
+    </div>
 </form>
 
 
