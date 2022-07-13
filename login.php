d9580391e4aa7135488b53799d8505fa071bec5d
CuddlyBunion341 <daniel@bengl.cz>
2022-07-13 21:41:12 +0200

    created a simple login form

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php require('templates/header.php'); ?>
+
+<?php
+
+?>
+
+
+<form action="login.php" method="post">
+    <h1>Log In</h1>
+    <input type="text" name="username" id="username" placeholder="Username">
+    <input type="password" name="password" id="password" placeholder="Password">
+</form>
+
+
+<?php require('templates/footer.php'); ?>
