ec5ec4b483fbf1314323f315c391a11d8f40845d
CuddlyBunion341 <daniel@bengl.cz>
2022-07-25 19:19:37 +0200

    generated pfps are now scaled correctly

--- js/register.js
+++ js/register.js
@@ -1,4 +1,5 @@
 function createPfp() {
+	// big number to be used in image generation
 	let hash = Number(
 		Math.random().toString().slice(2, 15) +
 			Math.random().toString().slice(2, 15)
@@ -6,9 +7,14 @@ function createPfp() {
 
 	const canvas = document.createElement("canvas");
 	const size = 6;
-	canvas.width = canvas.height = size;
+	canvas.width = canvas.height = 128;
 	const ctx = canvas.getContext("2d");
 
+	// set white background
+	ctx.fillStyle = "#FFF";
+	ctx.fillRect(0, 0, 128, 128);
+
+	// set pfp color
 	const colors = [
 		"#2196F3",
 		"#32c787",
@@ -23,22 +29,24 @@ function createPfp() {
 		"#f39c12",
 		"#d4e157",
 	];
-
 	ctx.fillStyle = colors[hash % 10];
+	ctx.scale(16, 16);
+
+	// add and mirror pixels
 	for (let i = 0; i < size / 2; i++) {
 		for (let j = 0; j < size; j++) {
 			hash = Math.floor(hash / 10);
 			if (hash % 10 < 3) {
-				ctx.fillRect(i, j, 1, 1);
-				ctx.fillRect((size - i - 1) * 1, j, 1, 1);
+				ctx.fillRect(i + 1, j + 1, 1, 1);
+				ctx.fillRect((size - i) * 1, j + 1, 1, 1);
 			}
 		}
 	}
 	return canvas.toDataURL();
 }
 
-$('form').on('submit',() => {
-    const dataUrl = createPfp();
-    $('#pfp').value(dataUrl);
-    return true;
-})
+$("form").on("submit", () => {
+	const dataUrl = createPfp();
+	$("#pfp").value(dataUrl);
+	return true;
+});
