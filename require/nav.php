040f98c560ad5d7268bcd371944433fa253010c4
CuddlyBunion341 <daniel@bengl.cz>
2022-08-11 23:16:53 +0200

    fixed navbar aligment

--- require/nav.php
+++ require/nav.php
@@ -1,27 +1,28 @@
 <nav>
-    <a href="index.php">Greendit</a>
-    <input type="text" placeholder="search..." id="search" autocomplete="off">
-    <?php
-    session_start();
-    if (isset($_SESSION['username'], $_SESSION['user_id'])) :
-        $username = $_SESSION['username'];
-    ?>
-        <label for="check" class="menu-icon">
-            &#9776;
-        </label>
-        <input type="checkbox" name="check" id="check">
-        <div class="menu">
-            <a href="post.php">Post</a>
-            <a href="create.php">Create</a>
-            <a href="logout.php">Logout</a>
-            <a href="users/<?= $username ?>">u/<?= $username ?></a>
-        </div>
+    <ul>
+        <li><a href="index.php">Greendit</a></li>
+        <li class="search-wrapper"><input type="text" placeholder="search..." id="search" autocomplete="off"></li>
+        <?php
+        session_start();
+        if (isset($_SESSION['username'], $_SESSION['user_id'])) :
+            $username = $_SESSION['username'];
+        ?>
+            <li class="menu-icon"><label for="check">
+                    &#9776;
+                </label></li>
+            <input type="checkbox" name="check" id="check">
+            <ul class="menu">
+                <li><a href="post.php">Post</a></li>
+                <li><a href="create.php">Create</a></li>
+                <li><a href="logout.php">Logout</a></li>
+                <li><a href="users/<?= $username ?>">u/<?= $username ?></a></li>
+            </ul>
 
-    <?php else : ?>
-        <a href="login.php">Login</a>
-        <a href="register.php">SignUp</a>
-    <?php endif; ?>
-
+        <?php else : ?>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">SignUp</a></li>
+        <?php endif; ?>
+    </ul>
 </nav>
 <div id="search-result__wrapper" class="hidden">
     <div id="search-result__content" class="article"></div>
