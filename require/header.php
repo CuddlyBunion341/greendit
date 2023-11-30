22a4ae91a4bafe945e79cd4ea3253ac1f685b384
Daniel Bengl <daniel.bengl@renuo.ch>
2023-11-30 12:19:57 +0100

    attempt fix base url

--- require/header.php
+++ require/header.php
@@ -14,7 +14,6 @@ if (!isset($_SESSION)) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Greendit is a lightweight and open source version of reddit using PHP and MySQL">
     <title>Greendit</title>
-    <base href="/greendit/" />
     <script src="js/myquery.js"></script>
     <script src="js/main.js" defer></script>
     <link rel="stylesheet" href="stylesheets/main.min.css">
